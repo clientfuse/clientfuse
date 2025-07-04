904ac0cf479508a529caa692daee5790460fd872
OBezpalko <olegbespalko12@gmail.com>
2025-07-04 19:45:43 +0200

    refactor: Clean up account-example.ts by removing unused properties
    - Removed redundant properties from the account object to streamline the code.
    - Simplified the structure for better readability and maintainability.

--- apps/api/src/app/account-example.ts
+++ apps/api/src/app/account-example.ts
@@ -6,122 +6,6 @@ export const account = {
     'googleEmail': 'olegbespalko12@gmail.com',
     'role': 'Manager',
     'email': 'olegbespalko12@gmail.com',
-    'hasClickedYouTubeOnboardingLink': true,
-    'agency': {
-      '_id': '673b9e0bc0b7299ff2953905',
-      'uuid': 'f6104c484d095739',
-      'email': 'olegbespalko12@gmail.com',
-      'googleGrantedScopes': [],
-      'hasConfiguredStandardAccessLinks': true,
-      'facebookAutoAssignUsers': [
-        {
-          'id': '1120153453176547',
-          'name': 'Oleg Bespalko',
-          'role': 'ADMIN'
-        }
-      ],
-      'googleTagManagerEmail': 'olegbespalko12@gmail.com',
-      'isGoogleTagManagerOnDefaultBusinessRequest': true,
-      'isGoogleTagManagerWithManageUserPermission': true,
-      'googleAnalyticsEmail': 'olegbespalko12@gmail.com',
-      'isGoogleAnalyticsOnDefaultBusinessRequest': true,
-      'isGoogleAnalyticsWithManageUserPermission': true,
-      'googleMyBusinessEmailOrId': 'olegbespalko12@gmail.com',
-      'isGoogleMyBusinessOnDefaultBusinessRequest': true,
-      'isGoogleMyBusinessWithManageUserPermission': true,
-      'googleAdsAccountEmail': 'olegbespalko12@gmail.com',
-      'isGoogleAdsOnDefaultBusinessRequest': true,
-      'isGoogleAdsWithManageUserPermission': true,
-      'googleAdsMethod': 'Email',
-      'googleMerchantCenterEmail': 'olegbespalko12@gmail.com',
-      'isGoogleMerchantCenterOnDefaultBusinessRequest': true,
-      'isGoogleMerchantCenterWithManageUserPermission': true,
-      'googleSearchConsoleEmail': 'olegbespalko12@gmail.com',
-      'isGoogleSearchConsoleOnDefaultBusinessRequest': true,
-      'didNotGrantGoogleSearchConsoleScope': false,
-      'isGoogleSearchConsoleWithManageUserPermission': true,
-      'platformsToRequest': {
-        'YouTube': {
-          'platform': 'YouTube',
-          'assetTypes': [
-            {
-              'type': 'YouTube Channel',
-              'isEnabled': false,
-              '_id': '673ba266c0b7299ff29819bf'
-            }
-          ],
-          'agencyAccountId': 'olegbespalko12@gmail.com',
-          'agencyAccountName': 'olegbespalko12@gmail.com',
-          '_id': '673ba266c0b7299ff29819c0'
-        }
-      },
-      '__v': 1,
-      'googleBusinessProfileAccounts': [
-        {
-          'id': '118316088613841246190',
-          'name': 'Oleg Bespalko',
-          'accountType': 'PERSONAL',
-          '_id': '673b9e0bc0b7299ff29538ff'
-        }
-      ],
-      'billingAccount': {
-        'stripeSubscriptionStatus': {
-          'leadsiePackage': {
-            'name': 'Free Trial',
-            'status': 'active',
-            'credits': null,
-            'creditsViewOnly': null,
-            'rolloverCredits': 0,
-            'rolloverCreditsViewOnly': 0,
-            'isTeamsEnabled': true,
-            'isWhiteLabelEnabled': true,
-            'startDate': '2024-11-18T00:00:00.000Z',
-            'hasExpired': false,
-            'thisPeriodStartDate': '2024-11-18T00:00:00.000Z',
-            'thisPeriodEndDate': '2024-12-18T17:19:21.956Z',
-            'cancelAt': '2024-12-18T17:19:21.956Z',
-            'endedAt': null,
-            'isStripeCustomer': false,
-            'availableCredits': [
-              {
-                'agency': '673b9e0bc0b7299ff2953905',
-                'startDate': '2024-11-18T00:00:00.000Z',
-                'expiryDate': '2024-12-18T17:19:21.956Z',
-                'creditsCount': null,
-                'creditsViewOnlyCount': null
-              }
-            ],
-            'overageCredits': 0,
-            'overageCreditsViewOnly': 0
-          }
-        },
-        'trialEndDate': '2024-12-18T17:19:21.956Z',
-        'pauses': [],
-        'oldStripeCheckoutSessionIds': [],
-        '_id': '6756cefd8cb46d86e1ec7571'
-      },
-      'assets': [
-        'Ad Account',
-        'Page',
-        'Instagram Account',
-        'Pixel',
-        'Catalog'
-      ],
-      'didNotGrantGoogleMyBusinessScope': false,
-      'googleBusinessProfileAccountId': null,
-      'googleBusinessProfileAccountName': null,
-      'googleBusinessProfileAccountNumber': null,
-      'googleBusinessProfileOrganizationId': null,
-      'hasExtraDomainSharingInstructions': false,
-      'hasExtraLeadsAccessSharingInstructions': false,
-      'isFacebookOnDefaultBusinessRequest': true,
-      'mainFacebookBusinessManagerId': '191843779016692',
-      'mainFacebookBusinessManagerName': 'Old Business',
-      'facebookIsEnabled': true,
-      'facebookBusinessManagerId': '191843779016692',
-      'facebookBusinessManagerName': 'Old Business',
-      'granularPermissions': {}
-    },
     'auth': {
       'YouTube': {
         'platform': 'YouTube',
@@ -523,7 +407,6 @@ export const account = {
       }
     ],
     'apps': [],
-    'appSumoCode': false,
     'isLoggedInWithFacebook': true,
     'isLoggedInWithGoogle': true,
     'isLoggedInWithPlatforms': [
